1f3f65d9df78f2f2fe9eb34b2aace4c8e608934d
Christian Kozalla <christian.kozalla@gmail.com>
2024-03-18 21:11:04 +0100

    lazy load many primevue components

--- web-client/src/main.ts
+++ web-client/src/main.ts
@@ -2,7 +2,7 @@ import 'primevue/resources/themes/aura-light-indigo/theme.css';
 import './assets/main.css';
 import './assets/icons.css';
 
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
@@ -10,18 +10,11 @@ import router from './router';
 import PrimeVue from 'primevue/config';
 
 // Manually importing because usage of unplugin-vue-components infinitely triggers file-watcher pattern in modd.conf
-import InputText from 'primevue/inputtext';
-import Button from 'primevue/button';
 import Menu from 'primevue/menu';
 import Menubar from 'primevue/menubar';
-import TabMenu from 'primevue/tabmenu';
 import ProgressSpinner from 'primevue/progressspinner';
-import Toast from 'primevue/toast';
 import ToastService from 'primevue/toastservice';
-import Dropdown from 'primevue/dropdown';
-import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-import Panel from 'primevue/panel';
 
 const app = createApp(App);
 
@@ -29,17 +22,39 @@ app.use(createPinia());
 app.use(PrimeVue);
 app.use(ToastService);
 
-app.component('InputText', InputText);
-app.component('Button', Button);
+app.component(
+  'InputText',
+  defineAsyncComponent(() => import('primevue/inputtext'))
+);
+app.component(
+  'TabMenu',
+  defineAsyncComponent(() => import('primevue/tabmenu'))
+);
+app.component(
+  'Toast',
+  defineAsyncComponent(() => import('primevue/toast'))
+);
+app.component(
+  'DataTable',
+  defineAsyncComponent(() => import('primevue/datatable'))
+);
+app.component(
+  'Button',
+  defineAsyncComponent(() => import('primevue/button'))
+);
+app.component(
+  'Dropdown',
+  defineAsyncComponent(() => import('primevue/dropdown'))
+);
+app.component(
+  'Panel',
+  defineAsyncComponent(() => import('primevue/panel'))
+);
+
 app.component('Menu', Menu);
 app.component('Menubar', Menubar);
-app.component('TabMenu', TabMenu);
-app.component('ProgressSpinner', ProgressSpinner);
-app.component('Toast', Toast);
-app.component('Dropdown', Dropdown);
-app.component('DataTable', DataTable);
 app.component('Column', Column);
-app.component('Panel', Panel);
+app.component('ProgressSpinner', ProgressSpinner);
 
 app.use(router);
 
