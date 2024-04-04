0b17965d105bd9bd2e8b115fb60b2fa30d490f65
Christian Kozalla <christian.kozalla@gmail.com>
2024-04-04 20:57:48 +0200

    delete authStore because integrating pocketbase client is not suitable

--- web-client/src/api-client.ts
+++ web-client/src/api-client.ts
@@ -12,12 +12,8 @@ interface TypedPocketBase extends PocketBase {
 // import.meta.env.BASE_URL is provided by Vite (default "/") which works, because Vue and Pocketbase run on the same domain
 export const pb = new PocketBase(import.meta.env.BASE_URL) as TypedPocketBase;
 
-// Alternative implementation idea:
-// since it is problematic that pb and all its methods is not fully reactive and not integrated in Vue's reactivity system
-// and since pb.authStore.onChange handler must fire immediately, but then always for the root route ("/"), useRoute/useRouter composables are really usable inside
-// -> make a Pinia store, that initializes pb and then returns the instance, and a couple of Vue's reactive primitive only to keep the UI in sync with the auth state from pb
 export const isAuthenticated = ref<boolean>(false);
-pb.authStore.onChange((token, _model) => {
+pb.authStore.onChange((token) => {
   if (!token) {
     const currentLocale = supportedOrFallbackLocale(window.location.pathname.slice(1, 3));
     isAuthenticated.value = false;
