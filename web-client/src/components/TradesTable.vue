2d6b8c1a8813a46f9a6380fe6dc87a090ac8c7fc
Christian Kozalla <christian.kozalla@gmail.com>
2024-05-01 22:43:55 +0200

    calculate date range min max of trades and fetch screenshots in this range

--- web-client/src/components/TradesTable.vue
+++ web-client/src/components/TradesTable.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, type Ref, computed, onMounted } from 'vue';
+import { ref, type Ref, computed, onMounted, onUpdated } from 'vue';
 import { pb, type Screenshot as ScreenshotType } from '@/api-client';
 import Screenshot from '@/components/Screenshot.vue';
 import { type usePaginatedCollection } from '@/composables/usePaginatedCollection';
@@ -14,23 +14,52 @@ type ScreenshotsByDate = { [date: string]: ScreenshotType };
 const items = computed(() => {
   return (props.trades.collection?.items || []).map(props.mapperFn);
 });
+const dateRange = computed(() => getDateRange(items.value));
 const screenshots = ref<ScreenshotsByDate>({});
+
+// this could be implemented with a watcher as well
 onMounted(fetchAndUpdateScreenshots(screenshots));
+onUpdated(fetchAndUpdateScreenshots(screenshots));
 
 function fetchAndUpdateScreenshots(ref: Ref<ScreenshotsByDate>) {
   return () => {
-    pb.collection('screenshots')
-      .getList(undefined, undefined, {
-        skipTotal: true,
-        filter: pb.filter('date >= {:startDate} && date <= {:endDate}', {
-          startDate: new Date('2024-02-01'),
-          endDate: new Date('2024-03-01')
+    if (dateRange.value.min && dateRange.value.max) {
+      pb.collection('screenshots')
+        .getList(undefined, undefined, {
+          skipTotal: true,
+          filter: pb.filter('date >= {:startDate} && date <= {:endDate}', {
+            startDate: new Date(dateRange.value.min),
+            endDate: new Date(dateRange.value.max)
+          })
         })
-      })
-      .then((list) => {
-        for (const record of list.items) ref.value[record.date.slice(0, 10)] = record; // refactor task: each date should hold a list of records - i.e. { [date: string]: ScreenshotType[] }
-      });
+        .then((list) => {
+          for (const record of list.items) ref.value[record.date.slice(0, 10)] = record; // refactor task: each date should hold a list of records - i.e. { [date: string]: ScreenshotType[] }
+        });
+    }
   };
+}
+
+// https://stackoverflow.com/questions/14781153/how-to-compare-two-string-dates-in-javascript
+function isLater(date1: string, date2: string) {
+  return date1 > date2; // date1 is later than date2 - e.g. 2024-02-10 is later than 2023-11-30
+}
+function isEarlier(date1: string, date2: string) {
+  return date1 < date2;
+}
+
+function getDateRange(list: { DateTime_close: string }[]): { min?: string; max?: string } {
+  let min = list[0]?.DateTime_close.slice(0, 10);
+  let max = list[0]?.DateTime_close.slice(0, 10);
+
+  for (const { DateTime_close } of list) {
+    const currentDate = DateTime_close.slice(0, 10);
+    if (isEarlier(currentDate, min)) {
+      min = currentDate;
+    } else if (isLater(currentDate, max)) {
+      max = currentDate;
+    }
+  }
+  return { min, max };
 }
 </script>
 
