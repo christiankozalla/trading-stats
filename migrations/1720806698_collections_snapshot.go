a26a1a3120f96b2599004da316e10ff38d9815ea
Christian Kozalla <christian.kozalla@gmail.com>
2024-07-12 20:01:07 +0200

    chore: collections snapshot + cascadeDelete true for user or trading-account

--- migrations/1720806698_collections_snapshot.go
+++ migrations/1720806698_collections_snapshot.go
@@ -13,69 +13,6 @@ func init() {
 	m.Register(func(db dbx.Builder) error {
 		jsonData := `[
 			{
-				"id": "_pb_users_auth_",
-				"created": "2024-03-03 19:42:42.123Z",
-				"updated": "2024-03-03 19:42:42.124Z",
-				"name": "users",
-				"type": "auth",
-				"system": false,
-				"schema": [
-					{
-						"system": false,
-						"id": "users_name",
-						"name": "name",
-						"type": "text",
-						"required": false,
-						"presentable": false,
-						"unique": false,
-						"options": {
-							"min": null,
-							"max": null,
-							"pattern": ""
-						}
-					},
-					{
-						"system": false,
-						"id": "users_avatar",
-						"name": "avatar",
-						"type": "file",
-						"required": false,
-						"presentable": false,
-						"unique": false,
-						"options": {
-							"mimeTypes": [
-								"image/jpeg",
-								"image/png",
-								"image/svg+xml",
-								"image/gif",
-								"image/webp"
-							],
-							"thumbs": null,
-							"maxSelect": 1,
-							"maxSize": 5242880,
-							"protected": false
-						}
-					}
-				],
-				"indexes": [],
-				"listRule": "id = @request.auth.id",
-				"viewRule": "id = @request.auth.id",
-				"createRule": "",
-				"updateRule": "id = @request.auth.id",
-				"deleteRule": "id = @request.auth.id",
-				"options": {
-					"allowEmailAuth": true,
-					"allowOAuth2Auth": true,
-					"allowUsernameAuth": true,
-					"exceptEmailDomains": null,
-					"manageRule": null,
-					"minPasswordLength": 8,
-					"onlyEmailDomains": null,
-					"onlyVerified": false,
-					"requireEmail": false
-				}
-			},
-			{
 				"id": "zml6ffhmc1kewy5",
 				"created": "2024-03-09 20:58:00.947Z",
 				"updated": "2024-03-11 20:21:21.545Z",
@@ -140,7 +77,7 @@ func init() {
 						"unique": false,
 						"options": {
 							"collectionId": "_pb_users_auth_",
-							"cascadeDelete": false,
+							"cascadeDelete": true,
 							"minSelect": null,
 							"maxSelect": 1,
 							"displayFields": null
@@ -156,7 +93,7 @@ func init() {
 						"unique": false,
 						"options": {
 							"collectionId": "zml6ffhmc1kewy5",
-							"cascadeDelete": false,
+							"cascadeDelete": true,
 							"minSelect": null,
 							"maxSelect": 1,
 							"displayFields": null
@@ -204,7 +141,7 @@ func init() {
 			{
 				"id": "ux1jly2k6sai33h",
 				"created": "2024-03-12 07:28:09.858Z",
-				"updated": "2024-03-21 06:52:43.665Z",
+				"updated": "2024-07-12 17:50:36.312Z",
 				"name": "raw_trades",
 				"type": "base",
 				"system": false,
@@ -219,7 +156,7 @@ func init() {
 						"unique": false,
 						"options": {
 							"collectionId": "_pb_users_auth_",
-							"cascadeDelete": false,
+							"cascadeDelete": true,
 							"minSelect": null,
 							"maxSelect": 1,
 							"displayFields": null
@@ -235,7 +172,7 @@ func init() {
 						"unique": false,
 						"options": {
 							"collectionId": "zml6ffhmc1kewy5",
-							"cascadeDelete": false,
+							"cascadeDelete": true,
 							"minSelect": null,
 							"maxSelect": 1,
 							"displayFields": null
@@ -661,8 +598,8 @@ func init() {
 					}
 				],
 				"indexes": [
-					"CREATE INDEX ` + "`" + `idx_trades_InternalOrderID` + "`" + ` ON ` + "`" + `raw_trades` + "`" + ` (` + "`" + `InternalOrderID` + "`" + `)",
-					"CREATE INDEX ` + "`" + `idx_trades_ParentInternalOrderID` + "`" + ` ON ` + "`" + `raw_trades` + "`" + ` (` + "`" + `ParentInternalOrderID` + "`" + `)",
+					"CREATE UNIQUE INDEX ` + "`" + `idx_trades_InternalOrderID` + "`" + ` ON ` + "`" + `raw_trades` + "`" + ` (\n  ` + "`" + `InternalOrderID` + "`" + `,\n  ` + "`" + `account` + "`" + `\n)",
+					"CREATE INDEX ` + "`" + `idx_trades_ParentInternalOrderID` + "`" + ` ON ` + "`" + `raw_trades` + "`" + ` (\n  ` + "`" + `ParentInternalOrderID` + "`" + `,\n  ` + "`" + `account` + "`" + `\n)",
 					"CREATE INDEX ` + "`" + `idx_trades_OpenClose` + "`" + ` ON ` + "`" + `raw_trades` + "`" + ` (` + "`" + `OpenClose` + "`" + `)"
 				],
 				"listRule": "@request.auth.id != \"\" && @request.auth.id = user.id",
@@ -675,7 +612,7 @@ func init() {
 			{
 				"id": "1q29zijc1iib3z8",
 				"created": "2024-03-21 07:37:11.677Z",
-				"updated": "2024-03-22 15:29:39.266Z",
+				"updated": "2024-07-12 17:50:36.384Z",
 				"name": "trades",
 				"type": "base",
 				"system": false,
@@ -716,7 +653,7 @@ func init() {
 						"unique": false,
 						"options": {
 							"collectionId": "_pb_users_auth_",
-							"cascadeDelete": false,
+							"cascadeDelete": true,
 							"minSelect": null,
 							"maxSelect": 1,
 							"displayFields": null
@@ -732,7 +669,7 @@ func init() {
 						"unique": false,
 						"options": {
 							"collectionId": "zml6ffhmc1kewy5",
-							"cascadeDelete": false,
+							"cascadeDelete": true,
 							"minSelect": null,
 							"maxSelect": 1,
 							"displayFields": null
@@ -884,6 +821,298 @@ func init() {
 				"updateRule": "@request.auth.id != \"\" && @request.auth.id = user.id",
 				"deleteRule": "@request.auth.id != \"\" && @request.auth.id = user.id",
 				"options": {}
+			},
+			{
+				"id": "pteexcmbctdt6u8",
+				"created": "2024-03-22 18:18:38.923Z",
+				"updated": "2024-07-12 17:50:36.396Z",
+				"name": "profit_loss",
+				"type": "view",
+				"system": false,
+				"schema": [
+					{
+						"system": false,
+						"id": "kkfxrib5",
+						"name": "user",
+						"type": "relation",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"collectionId": "_pb_users_auth_",
+							"cascadeDelete": true,
+							"minSelect": null,
+							"maxSelect": 1,
+							"displayFields": null
+						}
+					},
+					{
+						"system": false,
+						"id": "q65fzzzr",
+						"name": "account",
+						"type": "relation",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"collectionId": "zml6ffhmc1kewy5",
+							"cascadeDelete": true,
+							"minSelect": null,
+							"maxSelect": 1,
+							"displayFields": null
+						}
+					},
+					{
+						"system": false,
+						"id": "h9mypxua",
+						"name": "DateTime_close",
+						"type": "date",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"min": "",
+							"max": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "keptdwja",
+						"name": "ProfitLoss_dollar",
+						"type": "json",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"maxSize": 1
+						}
+					}
+				],
+				"indexes": [],
+				"listRule": "account.public_dashboard_permissions_via_account.is_trades_table_public = true || @request.auth.id = user.id",
+				"viewRule": "account.public_dashboard_permissions_via_account.is_trades_table_public = true || @request.auth.id = user.id",
+				"createRule": null,
+				"updateRule": null,
+				"deleteRule": null,
+				"options": {
+					"query": "SELECT\n  id,\n  user,\n  account,\n  DateTime_close,\n  (ProfitLoss * Multiplier) AS ProfitLoss_dollar\nFROM trades;"
+				}
+			},
+			{
+				"id": "il1hgvyunyj2rse",
+				"created": "2024-04-21 15:10:55.003Z",
+				"updated": "2024-07-12 17:50:36.333Z",
+				"name": "screenshots",
+				"type": "base",
+				"system": false,
+				"schema": [
+					{
+						"system": false,
+						"id": "bsn1sajc",
+						"name": "image",
+						"type": "file",
+						"required": true,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"mimeTypes": [
+								"image/png",
+								"image/jpeg"
+							],
+							"thumbs": [
+								"100x100",
+								"0x40"
+							],
+							"maxSelect": 1,
+							"maxSize": 2097152,
+							"protected": false
+						}
+					},
+					{
+						"system": false,
+						"id": "d99getyj",
+						"name": "date",
+						"type": "date",
+						"required": true,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"min": "",
+							"max": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "978g8nyy",
+						"name": "comment",
+						"type": "text",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"pattern": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "3dlovchr",
+						"name": "account",
+						"type": "relation",
+						"required": true,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"collectionId": "zml6ffhmc1kewy5",
+							"cascadeDelete": true,
+							"minSelect": null,
+							"maxSelect": 1,
+							"displayFields": null
+						}
+					},
+					{
+						"system": false,
+						"id": "bia4eiuu",
+						"name": "imageHeight",
+						"type": "number",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"noDecimal": false
+						}
+					},
+					{
+						"system": false,
+						"id": "znqukfa3",
+						"name": "imageWidth",
+						"type": "number",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"noDecimal": false
+						}
+					}
+				],
+				"indexes": [],
+				"listRule": "@request.auth.id != \"\" && @request.auth.id = account.user.id",
+				"viewRule": "@request.auth.id != \"\" && @request.auth.id = account.user.id",
+				"createRule": "@request.auth.id != \"\"",
+				"updateRule": "@request.auth.id != \"\" && @request.auth.id = account.user.id",
+				"deleteRule": "@request.auth.id != \"\" && @request.auth.id = account.user.id",
+				"options": {}
+			},
+			{
+				"id": "pn3cdlwnxlnrx87",
+				"created": "2024-06-19 07:18:43.274Z",
+				"updated": "2024-07-12 17:50:36.377Z",
+				"name": "public_dashboard_permissions",
+				"type": "base",
+				"system": false,
+				"schema": [
+					{
+						"system": false,
+						"id": "qyo3cfbv",
+						"name": "account",
+						"type": "relation",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"collectionId": "zml6ffhmc1kewy5",
+							"cascadeDelete": true,
+							"minSelect": null,
+							"maxSelect": 1,
+							"displayFields": null
+						}
+					},
+					{
+						"system": false,
+						"id": "yw3yp4op",
+						"name": "is_trades_table_public",
+						"type": "bool",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {}
+					}
+				],
+				"indexes": [],
+				"listRule": "",
+				"viewRule": "",
+				"createRule": "@request.auth.id != \"\"",
+				"updateRule": "@request.auth.id != \"\" && @request.auth.id = account.user.id",
+				"deleteRule": "@request.auth.id != \"\" && @request.auth.id = account.user.id",
+				"options": {}
+			},
+			{
+				"id": "_pb_users_auth_",
+				"created": "2024-07-12 17:50:36.242Z",
+				"updated": "2024-07-12 17:50:36.258Z",
+				"name": "users",
+				"type": "auth",
+				"system": false,
+				"schema": [
+					{
+						"system": false,
+						"id": "users_name",
+						"name": "name",
+						"type": "text",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"pattern": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "users_avatar",
+						"name": "avatar",
+						"type": "file",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"mimeTypes": [
+								"image/jpeg",
+								"image/png",
+								"image/svg+xml",
+								"image/gif",
+								"image/webp"
+							],
+							"thumbs": null,
+							"maxSelect": 1,
+							"maxSize": 5242880,
+							"protected": false
+						}
+					}
+				],
+				"indexes": [],
+				"listRule": "id = @request.auth.id",
+				"viewRule": "id = @request.auth.id",
+				"createRule": "",
+				"updateRule": "id = @request.auth.id",
+				"deleteRule": "id = @request.auth.id",
+				"options": {
+					"allowEmailAuth": true,
+					"allowOAuth2Auth": true,
+					"allowUsernameAuth": true,
+					"exceptEmailDomains": null,
+					"manageRule": null,
+					"minPasswordLength": 8,
+					"onlyEmailDomains": null,
+					"onlyVerified": false,
+					"requireEmail": false
+				}
 			}
 		]`
 
